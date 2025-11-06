069f4a5769ac3279707caf80e629fb28aa07e17d
glassfish21 <tjdwlswkd21@gmail.com>
2025-11-06 22:47:34 +0900

    deletecppprj7

--- cppprj7/cppprj7-1/README.md
+++
@@ -1,1 +0,0 @@
-<img width="1418" height="516" alt="image" src="https://github.com/user-attachments/assets/e7ea7fc0-2b1e-48ff-a02a-b2a0874b9f79" />
--- cppprj7/cppprj7-1/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "complex.h"
-
-int main() {
-    complex x(2, 3), y(-5, 10), sum;
-    sum = complexadd(x, y);
-    sum.print();
-    return 0;
-}
